0d925a3c1639870b6ef19557891f9bcc4f8e573d
Riya Jain <riya1jain567@gmail.com>
2025-07-19 04:37:42 +0530

    Add down migration script to drop existing tables

---
+++ data/migrations/down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS api_keys;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS project_members;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS organization_members;
+DROP TABLE IF EXISTS organizations;
+DROP TABLE IF EXISTS accounts;
