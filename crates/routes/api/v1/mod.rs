5e0f6865b40ac909da9bfa4d8755e7403f6f2bac
Riya Jain <riya1jain567@gmail.com>
2025-07-19 15:17:39 +0530

    Add organization routes with create and retrieve functionality

--- crates/routes/api/v1/mod.rs
+++ crates/routes/api/v1/mod.rs
@@ -1,7 +1,11 @@
 use actix_web::web;
 
 pub mod auth;
+pub mod me;
+pub mod orgs;
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/auth").configure(auth::routes));
+    cfg.service(web::scope("/auth").configure(auth::routes))
+        .service(web::scope("/me").configure(me::routes))
+        .service(web::scope("/orgs").configure(orgs::routes));
 }
