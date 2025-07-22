5adbe08d69417b731d9e72b1a94505adea2d179f
jriyyya <riya1jain567@gmail.com>
2025-07-22 17:39:00 +0530

    Add v1beta API routes configuration to the service

--- crates/routes/api/mod.rs
+++ crates/routes/api/mod.rs
@@ -1,7 +1,9 @@
 use actix_web::web;
 
 pub mod v1;
+pub mod v1beta;
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("/v1").configure(v1::routes));
+    cfg.service(web::scope("/v1beta").configure(v1beta::routes));
 }
