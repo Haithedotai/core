ee8412a18414787e68554ca47450aaaaee7f8c60
Spandan Barve <spandan567@gmail.com>
2025-07-31 20:23:13 +0530

    contract refactor

--- packages/contracts/src/tUSDT.sol
+++ packages/contracts/src/tUSDT.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract tusdt is ERC20 {
-    constructor() ERC20("testUSDC", "tUSDC") {
+contract tUSDT is ERC20 {
+    constructor() ERC20("testUSDT", "tUSDT") {
         _mint(msg.sender, 100_000_000 * 10 ** decimals());
     }
 }
