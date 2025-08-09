21f4fd9428f820b319d5b2a3fd378f0a15c55e91
Spandan Barve <spandan567@gmail.com>
2025-08-09 14:40:27 +0530

    sync down with up sql migrations

--- cargo/data/migrations/down.sql
+++ cargo/data/migrations/down.sql
@@ -1,7 +1,28 @@
-DROP TABLE IF EXISTS api_keys;
+-- Drop tables in reverse order to handle foreign key dependencies
+DROP TABLE IF EXISTS agent_preview_messages;
+
+DROP TABLE IF EXISTS agent_preview_conversations;
+
+DROP TABLE IF EXISTS conversations;
+
+DROP TABLE IF EXISTS faucet_requests;
+
+DROP TABLE IF EXISTS project_products_enabled;
+
 DROP TABLE IF EXISTS products;
+
+DROP TABLE IF EXISTS creators;
+
+DROP TABLE IF EXISTS org_model_enrollments;
+
 DROP TABLE IF EXISTS project_members;
+
 DROP TABLE IF EXISTS projects;
-DROP TABLE IF EXISTS organization_members;
+
+DROP TABLE IF EXISTS org_members;
+
 DROP TABLE IF EXISTS organizations;
+
+DROP TABLE IF EXISTS sessions;
+
 DROP TABLE IF EXISTS accounts;
