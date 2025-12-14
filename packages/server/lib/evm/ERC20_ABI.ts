9da7bbad9597d8dc864f3a7c143277b3b27518d1
Spandan Barve <spandan567@gmail.com>
2025-12-15 02:08:25 +0530

    Add ERC20_ABI definition for standard ERC20 functions

---
+++ packages/server/lib/evm/ERC20_ABI.ts
@@ -0,0 +1,78 @@
+export const ERC20_ABI = [
+	{
+		name: "transfer",
+		type: "function",
+		stateMutability: "nonpayable",
+		inputs: [
+			{ name: "to", type: "address" },
+			{ name: "value", type: "uint256" },
+		],
+		outputs: [{ type: "bool" }],
+	},
+	{
+		name: "transferFrom",
+		type: "function",
+		stateMutability: "nonpayable",
+		inputs: [
+			{ name: "from", type: "address" },
+			{ name: "to", type: "address" },
+			{ name: "value", type: "uint256" },
+		],
+		outputs: [{ type: "bool" }],
+	},
+	{
+		name: "approve",
+		type: "function",
+		stateMutability: "nonpayable",
+		inputs: [
+			{ name: "spender", type: "address" },
+			{ name: "value", type: "uint256" },
+		],
+		outputs: [{ type: "bool" }],
+	},
+	{
+		name: "allowance",
+		type: "function",
+		stateMutability: "view",
+		inputs: [
+			{ name: "owner", type: "address" },
+			{ name: "spender", type: "address" },
+		],
+		outputs: [{ type: "uint256" }],
+	},
+	{
+		name: "balanceOf",
+		type: "function",
+		stateMutability: "view",
+		inputs: [{ name: "account", type: "address" }],
+		outputs: [{ type: "uint256" }],
+	},
+	{
+		name: "totalSupply",
+		type: "function",
+		stateMutability: "view",
+		inputs: [],
+		outputs: [{ type: "uint256" }],
+	},
+	{
+		name: "name",
+		type: "function",
+		stateMutability: "view",
+		inputs: [],
+		outputs: [{ type: "string" }],
+	},
+	{
+		name: "symbol",
+		type: "function",
+		stateMutability: "view",
+		inputs: [],
+		outputs: [{ type: "string" }],
+	},
+	{
+		name: "decimals",
+		type: "function",
+		stateMutability: "view",
+		inputs: [],
+		outputs: [{ type: "uint8" }],
+	},
+];
