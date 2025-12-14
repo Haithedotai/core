44da7a0ca0c9b1e925fe040f2509afd090b76392
Spandan Barve <spandan567@gmail.com>
2025-12-15 00:10:34 +0530

    standard export from evm mcp index

--- packages/server/mcp/evm/index.ts
+++ packages/server/mcp/evm/index.ts
@@ -1,5 +1,21 @@
-// mcp/evm/index.ts
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import type { Hex } from "viem";
+import type { McpServerCreationConfig } from "../registry";
+import registerEvmTools from "./tools/_registerTools";
 
-export * from "./functions";
-export * from "./server";
-export * from "./viemClient";
+export type EvmMcpConfig = {
+	privateKey: Hex;
+};
+
+export default function (config: McpServerCreationConfig<EvmMcpConfig>) {
+	const { name, ...restConfig } = config;
+
+	const server = new McpServer({
+		version: "0.0.1",
+		name,
+	});
+
+	registerEvmTools({ server, ...restConfig });
+
+	return server;
+}
