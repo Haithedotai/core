816e30bfddf30bede759375fd93d0539552afaf6
Spandan Barve <spandan567@gmail.com>
2025-12-14 17:05:52 +0530

    bugfix

--- packages/server/routes/mcp/index.ts
+++ packages/server/routes/mcp/index.ts
@@ -12,15 +12,17 @@ mcpServer.connect(transport);
 
 const app = new Hono()
 
-	.get("/mcp", async (c) => {
-		console.log(JSON.stringify(c.req.raw.headers));
+	.get("/", async (c) => {
+		console.log("GET HEADERS : ", JSON.stringify(c.req.raw.headers));
 
 		await transport.handleRequest(c);
 		return c.text("OK");
 	})
 
-	.post("/mcp", async (c) => {
-		transport.handleRequest(c);
+	.post("/", async (c) => {
+		console.log("POST HEADERS : ", JSON.stringify(c.req.raw.headers));
+
+		await transport.handleRequest(c);
 		return c.text("OK");
 	});
 
