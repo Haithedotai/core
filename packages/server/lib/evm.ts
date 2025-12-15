4200eee0fd6461fe284467db20516308dc80da6b
Spandan Barve <spandan567@gmail.com>
2025-12-16 03:14:36 +0530

    evmclient for server

---
+++ packages/server/lib/evm.ts
@@ -0,0 +1,16 @@
+import { createWalletClient, http, isHex, publicActions } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import config from "../config";
+import env from "../env";
+
+if (!isHex(env.EVM_PRIVATE_KEY_SERVER)) {
+	throw new Error(
+		"env error: EVM_PRIVATE_KEY_SERVER is not a valid private key",
+	);
+}
+
+export const evmClient = createWalletClient({
+	chain: config.runtimeChain,
+	transport: http(config.runtimeChain.rpcUrls.default.http[0]),
+	account: privateKeyToAccount(env.EVM_PRIVATE_KEY_SERVER),
+}).extend(publicActions);
