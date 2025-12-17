3b2030b3a9424199f40b4693aff2af9c08801d43
Spandan Barve <spandan567@gmail.com>
2025-12-17 21:16:20 +0530

    add cookie names to jwtOptions and ensure cookie secret is included in cookieOptions

--- packages/server/config.ts
+++ packages/server/config.ts
@@ -8,12 +8,17 @@ const cookieOptions = {
 	secure: isProd,
 	sameSite: "Strict" as const,
 	path: "/",
+	secret: env.COOKIE_SIGNING_SECRET,
 };
 
 const jwtSecretBytes = new TextEncoder().encode(env.JWT_SECRET);
 const jwtOptions = {
 	secret: jwtSecretBytes,
 	algorithm: "HS256" as const,
+	cookieNames: {
+		access: "H_access_token",
+		refresh: "H_refresh_token",
+	},
 };
 
 const config = {
